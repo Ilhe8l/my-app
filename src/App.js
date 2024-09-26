01cc0b04eaf10579e5f30bc1f2c6b5efe6dd5124
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2024-09-26 10:12:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Olá, mundo!</h1>
+        <p>Este é um simples aplicativo React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
